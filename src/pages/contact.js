f186abcc224a7be63dfc5a0b86b67772e5208576
admin <mailtosungkwon@gmail.com>
2019-05-20 20:38:26 +0100

    Add Netlify contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,63 @@
-import React from "react"
+import React, { Component } from "react"
 import { graphql } from "gatsby"
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Layout from "../components/layout"
 
-export default ({ data }) => {
-  console.log("contact data", data)
-  const content = data.allContentfulContact.edges[0].node.bodytext.json
-  return (
-    <Layout>
-    <div>{documentToReactComponents(content)}</div>
-  </Layout>
-  )
+class Contact extends Component {
+
+  state = {
+    content: this.props.data.allContentfulContact.edges[0].node.bodytext.json,
+    name: '',
+    email: '',
+  }
+
+  handleInputChange = e => {
+    const target = e.target
+    const value = target.value
+    const name =  target.name
+
+    console.log("value", value)
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log(`hewwwwllo ${this.state.name}`)
+  }
+
+  render() {
+    return (
+      <Layout>
+        <form 
+          name="contact"
+          method="POST"
+          onSubmit={this.handleSubmit}
+          data-netlify="true" >
+          <label>
+            Name
+            <input 
+              type="text" 
+              name="name" 
+              value={this.state.name}
+              onChange={this.handleInputChange}
+              placeholder="Name" />
+
+          </label>
+          <textarea></textarea>
+          <button type="submit">Submit</button>
+        </form>
+        <div>{documentToReactComponents(this.state.content)}</div>
+      </Layout>
+    )
+  }
+
+
 }
+
+
+export default Contact
 
 export const query = graphql`
   query {
